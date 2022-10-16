bcd0a0d56100e28a6bec3c9a4e0fe146a3b4ea99
mnwachukwu042 <mnwachukwu042@gmail.com>
2022-10-16 02:57:32 +0100

    mapping farm budget and farm design data

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import { Stats } from "./Components";
 import Dashboard from "./Pages/Dashboard";
-import FarmBudgetForm from "./Pages/FarmBudgetForm";
-import FarmDesignBlocks from "./Pages/FarmDesignBlocks";
+import FarmBudget from "./Pages/FarmBudget";
+import FarmDesign from "./Pages/FarmDesign";
 
 function App() {
   return (
     <Routes>
       <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/farmBudgetForm" element={<FarmBudgetForm />} />
-      <Route path="/farmDesignBlocks" element={<FarmDesignBlocks />} />
+      <Route path="/farmBudget" element={<FarmBudget />} />
+      <Route path="/farmDesign" element={<FarmDesign />} />
       <Route path="/stat" element={<Stats />} />
     </Routes>
   );
